a223801dbe466d7602b50f029c332c46f5f80c29
Abubakr <abubakrolorunlambe@gmail.com>
2020-12-11 22:40:29 +0000

    Added a function that stores music title in a json file

--- commands/music_commands/play.js
+++ commands/music_commands/play.js
@@ -1,5 +1,7 @@
 const { Command } = require('discord.js-commando');
 const ytdl = require('ytdl-core');
+const fs = require('fs');
+const directory = 'C:/Users/ASidiq_ph1/Desktop/Personal Learning/DJ_ReloadIt/song_file.json';
 
 
 module.exports = class PlayMusic extends Command {
@@ -15,7 +17,7 @@ module.exports = class PlayMusic extends Command {
 					key: 'song',
 					prompt: 'What music would you like the DJ to play? Provide the link.',
 					type: 'string',
-					default: 'https://www.youtube.com/watch?v=hwZNL7QVJjE&list=PLnY6yPJxU6f9zIRM5uj2aifo1YnROESgb&index=79&ab_channel=SoulfulSounds',
+					default: 'https://www.youtube.com/watch?v=KCn8VmyObds&list=PLnY6yPJxU6f9zIRM5uj2aifo1YnROESgb&index=80&ab_channel=THEOSTENDMANPOPCORN%26SOUL',
 					throttling: {
 						usages: 3,
 						duration: 60,
@@ -41,9 +43,12 @@ module.exports = class PlayMusic extends Command {
 					const dispatcher = connection.play(ytdl(song,
 						{ filter: 'audioonly', quality: 'highestaudio' }));
 					dispatcher.setVolume(0.1);
+					// request to get music info from YouTube
 					ytdl.getBasicInfo(song).then(info => {
 						// Types: PLAYING, WATCHING, LISTENING, STREAMING,
 						this.client.user.setActivity(info.videoDetails.title, { type: 'PLAYING' });
+						// saves the title of the song in a file so it can be retrieved when the song is paused and then resumed again
+						saveFile(info.videoDetails.title);
 					});
 				});
 		}
@@ -52,3 +57,21 @@ module.exports = class PlayMusic extends Command {
 		}
 	}
 };
+
+function saveFile(content) {
+	// Creating JSON object
+	const songTitle = {
+		'title': content,
+	};
+
+	try{
+		// convert JSON object to a string
+		const data = JSON.stringify(songTitle, null, 4);
+
+		// write file to disk
+		fs.writeFileSync(directory, data, 'utf8');
+	}
+	catch (err) {
+		console.log(`Error writing file: ${err}`);
+	}
+}
