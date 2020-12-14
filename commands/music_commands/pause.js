9e30b2293b67c615f50a8e720c16aac9d243f098
Abubakr <abubakrolorunlambe@gmail.com>
2020-12-14 15:13:06 +0000

    Pause command now provides the right output if the command is used the music is already paused or none is being played

--- commands/music_commands/pause.js
+++ commands/music_commands/pause.js
@@ -12,14 +12,27 @@ module.exports = class PauseMusic extends Command {
 	}
 
 	run(message) {
+		// checks if member using command is in a voice channel
 		if (message.member.voice.channel) {
-			message.member.voice.channel.join()
-				.then(connection => {
-					// connection returns a VoiceConnection and dispatcher returns a StreamDispatcher
-					connection.dispatcher.pause([true]);
-					// Types: PLAYING, WATCHING, LISTENING, STREAMING,
-					this.client.user.setActivity('Music Paused...');
-				});
+			// stores current name of bot's activity
+			const activityName = this.client.user.presence.activities[0].name;
+			// if bot's activity is not both of the text, then it is playing music
+			if (activityName != 'Music Paused...' && activityName != 'with Commando') {
+				// bot joins the same voice channel as member or stays in if it is already there
+				message.member.voice.channel.join()
+					.then(connection => {
+						// connection returns a VoiceConnection and dispatcher returns a StreamDispatcher
+						connection.dispatcher.pause([true]);
+						// Types: PLAYING, WATCHING, LISTENING, STREAMING,
+						this.client.user.setActivity('Music Paused...');
+					});
+			}
+			else if (activityName === 'Music Paused...') {
+				message.reply('Music is paused already');
+			}
+			else {
+				message.reply('No music is currently playing');
+			}
 		}
 		else {
 			message.reply('You need to join a voice channel first!');
