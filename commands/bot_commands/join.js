21bc493207c634388eb25df768972c2b6f87c236
Abubakr <abubakrolorunlambe@gmail.com>
2020-12-21 19:28:36 +0000

    Created the folder for the bot centric commands and implemented the join command

---
+++ commands/bot_commands/join.js
@@ -0,0 +1,32 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class JoinVoiceChannel extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'join',
+			aliases: ['join', 'enter'],
+			group: 'bot_commands',
+			memberName: 'join',
+			description: 'Join a voice channel.',
+		});
+	}
+
+	run(message) {
+		const voiceChannel = message.member.voice.channel
+		// checks if member using command is in a voice channel
+		if (voiceChannel) {
+			// if the id(s) of voice channel(s) the bot is connected does not match member's
+			// voice channel id, join member's voice channel id
+			if (!this.client.voice.connections.some(conn => conn.channel.id === voiceChannel.id)) {
+				// bot joins the same voice channel as member
+				message.member.voice.channel.join();
+			}
+			else {
+				message.reply('DJ already in channel');
+			}
+		}
+		else {
+			message.reply('You need to join a voice channel first!');
+		}
+	}
+};
